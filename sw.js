5a942bb5aad6017e4a20cdadabacfcd93d286038
Ahmed Elsyed <ahmed2025elsyed@gmail.com>
2025-04-25 11:37:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,20 @@ self.addEventListener("install", function (e) {
   console.log("Service Worker Installed");
   e.waitUntil(
     caches.open("static").then(function (cache) {
-      return cache.addAll(["./", "./style.css", "./social_media. png"]);
+      return cache.addAll([
+        "./",
+        "./style.css",
+        "./social-media.png",
+        "./photo_5784886903068281808_x.jpg",
+        "./facebook (1).png",
+        "./twitter.png",
+        "./instagram.png",
+        "./youtube (1).png",
+        "./telegram.png",
+        "./tik-tok (1).png",
+        "./globe.png",
+        "./verified.png"
+      ]);
     })
   );
 });
